b6b13d47aaf1607ea5e3ecb5b2c3ea0b7610e99a
Landre306 <ludovicandre9@gmail.com>
2017-10-03 23:25:26 -0400

    Mise à jour de la classe display
    Je suis capable d'afficher les classes. Doit ajuster l'importation des méthode, attributs,sous-classes et détails.

--- tp1/src/Display.java
+++ tp1/src/Display.java
@@ -14,6 +14,8 @@ import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JList;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import parser.UcdSyntaxParser;
 import parser.UmlParsingError;
@@ -24,34 +26,24 @@ import javax.swing.UIManager;
 import java.awt.Color;
 import javax.swing.ListSelectionModel;
 import javax.swing.JTextArea;
+import javax.swing.DefaultListModel;
 import javax.swing.DropMode;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.JScrollBar;
+import javax.swing.JSlider;
 
 public class Display {
 
 	public JFrame frame;
 	private JTextField TextFichier;
-	private JTextField textField_4;
-	
+	private JTextField textDetails;
+	private UMLModel model;
 
 	/**
 	 * Launch the application.
 	 */
-	/*
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Display window = new Display();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	 */
+
 	/**
 	 * Create the application.
 	 */
@@ -86,7 +78,7 @@ public class Display {
 		frame.getContentPane().add(LabelAttribut);
 		
 		JLabel labelMethode = new JLabel("Méthodes");
-		labelMethode.setBounds(315, 95, 143, 14);
+		labelMethode.setBounds(370, 95, 143, 14);
 		frame.getContentPane().add(labelMethode);
 		
 		JLabel labelSousClasses = new JLabel("Sous-classes");
@@ -94,61 +86,95 @@ public class Display {
 		frame.getContentPane().add(labelSousClasses);
 		
 		JLabel labelAssociation = new JLabel("Associations/agrégations");
-		labelAssociation.setBounds(315, 263, 143, 14);
+		labelAssociation.setBounds(370, 263, 143, 14);
 		frame.getContentPane().add(labelAssociation);
 		
 		JLabel lblDetails = new JLabel("Détails");
 		lblDetails.setBounds(146, 394, 312, 14);
 		frame.getContentPane().add(lblDetails);
 		
-		textField_4 = new JTextField();
-		textField_4.setEditable(false);
-		textField_4.setBounds(146, 419, 312, 119);
-		frame.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		textDetails = new JTextField();
+		textDetails.setEditable(false);
+		textDetails.setBounds(146, 419, 428, 119);
+		frame.getContentPane().add(textDetails);
+		textDetails.setColumns(10);
 		
 		JPanel panelClasses = new JPanel();
 		panelClasses.setBorder(UIManager.getBorder("TextField.border"));
 		panelClasses.setBounds(23, 122, 113, 416);
 		frame.getContentPane().add(panelClasses);
 		
-		JList listClasses = new JList();
+		
+		
+		DefaultListModel<String> listC = new DefaultListModel<>();
+		JList listClasses =  new JList<>(listC);
+		listClasses.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting()) {
+                	textDetails.setText(null);
+                	//System.out.println(model);
+                	
+                   // textDetails.setText(model.classes.get(listClasses.getSelectedIndex()).toString() );
+                }
+            }
+        });
+		
+		
+		
+		listClasses.setValueIsAdjusting(true);
 		panelClasses.add(listClasses);
 		listClasses.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		listClasses.setBackground(Color.WHITE);
 		listClasses.setBorder(UIManager.getBorder("TextPane.border"));
 		
+		
+		
 		JPanel panelAttributs = new JPanel();
 		panelAttributs.setBorder(UIManager.getBorder("TextField.border"));
-		panelAttributs.setBounds(146, 120, 143, 112);
+		panelAttributs.setBounds(146, 120, 191, 112);
 		frame.getContentPane().add(panelAttributs);
 		
-		JList listAttributs = new JList();
+		DefaultListModel<String> listA = new DefaultListModel<>();
+		JList listAttributs = new JList<>(listA);
+		listAttributs.setValueIsAdjusting(true);
 		listAttributs.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		panelAttributs.add(listAttributs);
 		
+		
+
 		JPanel panelSousClasses = new JPanel();
 		panelSousClasses.setBorder(UIManager.getBorder("TextField.border"));
-		panelSousClasses.setBounds(146, 289, 143, 87);
+		panelSousClasses.setBounds(146, 289, 191, 87);
 		frame.getContentPane().add(panelSousClasses);
-		
-		JList listSousClasses = new JList();
+
+		DefaultListModel<String> listSC = new DefaultListModel<>();
+		JList listSousClasses = new JList<>(listSC);
+		listSousClasses.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listSousClasses.setValueIsAdjusting(true);
 		panelSousClasses.add(listSousClasses);
 		
 		JPanel panelMethodes = new JPanel();
 		panelMethodes.setBorder(UIManager.getBorder("TextField.border"));
-		panelMethodes.setBounds(315, 120, 143, 112);
+		panelMethodes.setBounds(370, 120, 204, 112);
 		frame.getContentPane().add(panelMethodes);
 		
-		JList listMethodes = new JList();
+		
+		DefaultListModel<String> listM = new DefaultListModel<>();
+		JList listMethodes = new JList<>(listM);
+		listMethodes.setValueIsAdjusting(true);
+		listMethodes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		panelMethodes.add(listMethodes);
 		
 		JPanel panelAssociation = new JPanel();
 		panelAssociation.setBorder(UIManager.getBorder("TextField.border"));
-		panelAssociation.setBounds(315, 289, 143, 87);
+		panelAssociation.setBounds(370, 289, 204, 87);
 		frame.getContentPane().add(panelAssociation);
 		
-		JList listAssociation = new JList();
+		DefaultListModel<String> listAss = new DefaultListModel<>();
+		JList listAssociation = new JList<>(listAss);
+		listAssociation.setValueIsAdjusting(true);
+		listAssociation.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		panelAssociation.add(listAssociation);
 		
 		JButton btnAjouterF = new JButton("Ajouter Fichier");
@@ -156,6 +182,8 @@ public class Display {
 		
 		btnAjouterF.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				
 				final JFileChooser fc = new JFileChooser();
 			    if (e.getSource() == btnAjouterF) {
 			        int returnVal = fc.showOpenDialog(TextFichier);
@@ -170,21 +198,46 @@ public class Display {
 			}
 		});
 		btnAjouterF.setBounds(394, 33, 119, 23);
+		
+		
 		frame.getContentPane().add(btnAjouterF);
 		JButton btnChargerFichier = new JButton("Charger fichier");
 		btnChargerFichier.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+
+	
+				
+				
+				listC.clear();
+				listAss.clear();
 				
 				try {
 					List<String> fileContent = FileReader.getFileContentFormatted(TextFichier.getText());
 					UMLModel model = UcdSyntaxParser.parse(fileContent);
 					System.out.println(model.toString());
+					
+
+					
+					for (int i =0 ; i<model.classes.size();i++)
+					{
+						listC.addElement(model.classes.get(i).getClassName());
+					}
+					for (int i =0; i<model.associations.size();i++)
+					{
+						
+						
+						listAss.addElement(model.associations.get(i).toString());
+					}
+					
+					
+					
 				} catch (IOException e) {
 					e.printStackTrace();
 					System.out.println("\n" + TextFichier.getText() + " is not a valid ucd file.");
 				} catch (UmlParsingError e) {
 					System.out.println(e.getMessage());
 				}
+				
 				
 			 
 			
