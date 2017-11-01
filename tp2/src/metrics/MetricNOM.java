1814de57dc6dfa40e86c9f00a749e3c8c5914ef4
Landre306 <ludovicandre9@gmail.com>
2017-10-31 20:32:38 -0400

    Implementation of metric NOA and NOM
    Implementation of the metric NOA and NOM.
    Still working on metric 4 to 10

--- tp2/src/metrics/MetricNOM.java
+++ tp2/src/metrics/MetricNOM.java
@@ -1,6 +1,9 @@
 package metrics;
 
+import java.util.List;
+
 import uml.UMLModel;
+import uml.uml_classes.Operation;
 import uml.uml_classes.UMLClass;
 
 public class MetricNOM extends BaseMetric {
@@ -14,7 +17,72 @@ public class MetricNOM extends BaseMetric {
 
 	@Override
 	public double compute(UMLModel m, UMLClass c) {
-		return 0.0;
+		
+		List<Operation> listOperation=c.getOperations();
+		List<UMLClass> listClass= m.getClasses();
+		float NOM= listOperation.size();
+		
+			for(int j=0;j<listClass.size();j++)
+			{
+				List<UMLClass> listSubClass= m.getSubclasses(listClass.get(j));
+				if(!listSubClass.isEmpty())
+				{
+					if(listOperation.size()!=0)
+					{
+						
+						for(int k=0;k<listSubClass.size();k++)
+						{
+							
+								if(listSubClass.get(k).getClassName()==c.getClassName())
+								{
+									NOM+=listClass.get(j).getOperations().size();
+									for(int i=0;i<listOperation.size();i++)
+									{
+										
+									
+											for(int l=0 ; l<listClass.get(j).getOperations().size();l++)
+											{
+												if(listClass.get(j).getOperations().get(l).getArgs().toString().contains(listOperation.get(i).getArgs().toString())
+														&& listClass.get(j).getOperations().get(l).getName()==listOperation.get(i).getName())
+												{
+													NOM-=1;
+												}
+												
+												
+											}
+				
+									}
+									
+								}
+								
+						}
+					}
+					else
+					{
+						
+						for(int k=0;k<listSubClass.size();k++)
+						{
+							
+								if(listSubClass.get(k).getClassName()==c.getClassName())
+								{									
+									NOM+=listClass.get(j).getOperations().size();
+								}
+						}
+				
+						
+					}
+				
+							
+				}
+					
+
+				}
+
+
+			
+		
+		return NOM;
+
 	}
 
 }
