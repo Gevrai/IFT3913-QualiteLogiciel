286ecbf966f89c4fccf139eb95cf568f8025099c
Landre306 <ludovicandre9@gmail.com>
2017-11-01 21:48:18 -0400

    Création des fichier test.ucd ainsi que leur valeur dans les classe java
    Push de la mise à jour du rapport et des diagramme uml pour faire les tests.

---
+++ tp2/src/tests/CLDTest.java
@@ -0,0 +1,97 @@
+package tests;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import metrics.MetricFactory;
+import parser.UcdSyntaxParser;
+import parser.UmlParsingError;
+import uml.UMLModel;
+import uml.uml_classes.UMLClass;
+import util.FileReader;
+
+public class CLDTest {
+	
+	UMLModel leagueModel;
+	UMLClass equipeClass;
+	
+	@Before
+	public void setUp() throws Exception {
+		try {
+			// Setting up valid league model from file
+			List<String> content = FileReader.getFileContent("./tests/JoueurTestCLD.ucd");
+			leagueModel = UcdSyntaxParser.parse(content);
+			equipeClass = leagueModel.getClassFromName("Joueur");
+			// Setting up other valid models can be done here, needs to add new global variables though
+		} catch (IOException e) { fail("./tests/JoueurTestCLD.ucd inexistant -> Cannot test");
+		} catch (UmlParsingError e) { 
+			e.printStackTrace();
+			fail("Error parsing valid file JoueurTestCLD.ucd"); }
+	}
+
+	// Helper function for tests
+	public void assertMetric(String acronym, String expectedResult, UMLModel m, UMLClass c) {
+		String result = MetricFactory.getMetric(acronym).computeToFormattedString(m, c);
+		assertEquals("Failed for model " + m.getModelName() + " on class " + c.getClassName(),
+				result , expectedResult);
+	}
+
+	@Test
+	public void MetricsANA_Test() { 
+		assertMetric("ANA", "0", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsNOM_Test() {
+		assertMetric("NOM", "4", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsNOA_Test() {
+		assertMetric("NOA", "3", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsITC_Test() {
+		assertMetric("ITC", "0", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsETC_Test() {
+		assertMetric("ETC", "2", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsCAC_Test() {
+		assertMetric("CAC", "0", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsDIT_Test() {
+		assertMetric("DIT", "1", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsCLD_Test() {
+		assertMetric("CLD", "2", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsNOC_Test() {
+		assertMetric("NOC", "1", leagueModel, equipeClass); 
+	}
+
+	@Test
+	public void MetricsNOD_Test() {
+		assertMetric("NOD", "0", leagueModel, equipeClass); 
+	}
+
+}
+
